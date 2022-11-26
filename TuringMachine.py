760b04bde30b5f90972a98c140bc0208fe711abf
KitekatTV <jfgleader@gmail.com>
2022-11-26 13:44:34 +0300

    Fixed 'if' command delay in step-by-step mode. Added some comments

--- TuringMachine.py
+++ TuringMachine.py
@@ -12,35 +12,32 @@ selectedIndex = 0 # Index of nums array element thats is currently under the poi
 numsArray = ['B']
 
 
+# Executes given command
 def Act(command: str) -> str:
-	# Этот язык явно придумал садист
 	global pointerPos
 	global selectedIndex
 	global numsArray
 
 	if command[0] == 'W':
 		numsArray[selectedIndex] = str(command[1])
-		DrawTerminal(numsArray, pointerPos, stepMode)
 	elif command[0] == 'R':
 		pointerPos += 1
 		selectedIndex += 1
 		if selectedIndex == len(numsArray):
 			numsArray.append('B')
-		DrawTerminal(numsArray, pointerPos, stepMode)
 	elif command[0] == 'L':
 		if selectedIndex == 0:
 			return "Halt"
 		else:
 			pointerPos -= 1
 			selectedIndex -= 1
-			DrawTerminal(numsArray, pointerPos, stepMode)
 	elif command[0] == 'I':
 		if command[1] == numsArray[selectedIndex]:
 			addcommands = command[3:-1].split('.')
 			for a in range(len(addcommands)):
 				if Act(addcommands[a]) == "Halt":
 					return "Halt"
-				if a != len(addcommands) - 1:
+				if a != len(addcommands) - 1 and not stepMode:
 					sleep(commandDelay)
 		else:
 			return "NoDelay"
@@ -56,30 +53,32 @@ def Act(command: str) -> str:
 			return "NoDelay"
 	elif command[0] == 'T':
 		return "Halt"
+
 	return "OK"
 
 
+# Calls function to execute given commands then calls function that draws in console
 def Run(commands: list):
 	global stepMode
 
 	DrawTerminal(numsArray, pointerPos,stepMode)
 	sleep(1)
+
 	for c in commands:
 		status = Act(c)
 		if status == "Halt":
+			DrawTerminal(numsArray, pointerPos, False, True)
 			break
+		elif status == "OK":
+			DrawTerminal(numsArray, pointerPos, stepMode)
 		if status != "NoDelay" and not stepMode:
 			sleep(commandDelay)
 	else:
 		while True:
 			DrawTerminal(numsArray, pointerPos,stepMode)
 
-	if status == "Halt":
-		DrawTerminal(None, None, False, True)
 
-
-
-
+# Parses terminal arguments
 def ParseArguments():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('path', type=str, help='Path to program')
@@ -87,9 +86,10 @@ def ParseArguments():
 	return Args
 
 
+# Program entry point
 def Entry():
 	global stepMode
-	stepMode=True
+	stepMode=True #DEBUG
 	path = ParseArguments().path
 
 	if not os.path.isfile(path):
